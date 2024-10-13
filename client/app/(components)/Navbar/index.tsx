1087eeb44c9e7a6de5c8e5f45ed5f081df27ef11
Yusin <sunkatabeckama@gmail.com>
2024-10-13 19:05:03 +0200

    Sidebar + DarkMode states passing

--- client/app/(components)/Navbar/index.tsx
+++ client/app/(components)/Navbar/index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Search, Settings } from "lucide-react";
+import { Menu, Search, Settings } from "lucide-react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/app/state";
 
 function Navbar() {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Search Bar  */}
       <div className="flex items-start gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
@@ -18,12 +32,12 @@ function Navbar() {
       </div>
 
       {/* Icons */}
-        <div className="flex items-center">
-            <Link href="/" className="h-min w-min rounded p-2 hover:bg-gray-100">
-                <Settings className="h-6 w-6 cursor-pointer dark:text-white"/>
-            </Link>
-            <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
-        </div>
+      <div className="flex items-center">
+        <Link href="/" className="h-min w-min rounded p-2 hover:bg-gray-100">
+          <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
+        </Link>
+        <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
+      </div>
     </div>
   );
 }
--- client/app/dashboardWrapper.tsx
+++ client/app/dashboardWrapper.tsx
@@ -1,17 +1,28 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
-import StoreProvider from "./redux";
+import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector((state)=>state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state)=>state.global.isDarkMode);
+
+  useEffect(()=>{
+    if (isDarkMode){
+      document.documentElement.classList.add("dark");
+    }else{
+      document.documentElement.classList.remove("dark");
+    }
+})
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
       <Sidebar />
       <main
-        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${ isSidebarCollapsed ? "" :  "md:pl-64"}`}
       >
         {/* navbar*/}
         <Navbar />
