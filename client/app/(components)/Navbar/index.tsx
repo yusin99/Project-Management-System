e92a9bab4284d1fa245a9e24f8dbf0af8775d4ce
Yusin <sunkatabeckama@gmail.com>
2024-10-13 19:11:11 +0200

    Dark mode set up

--- client/app/(components)/Navbar/index.tsx
+++ client/app/(components)/Navbar/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Menu, Search, Settings } from "lucide-react";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/app/state";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/app/state";
 
 function Navbar() {
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
   );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
@@ -33,7 +34,29 @@ function Navbar() {
 
       {/* Icons */}
       <div className="flex items-center">
-        <Link href="/" className="h-min w-min rounded p-2 hover:bg-gray-100">
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >
+          {" "}
+          {isDarkMode ? (
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}{" "}
+        </button>
+        <Link
+          href="/"
+          className={
+            isDarkMode
+              ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
+          }
+        >
           <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
         </Link>
         <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
