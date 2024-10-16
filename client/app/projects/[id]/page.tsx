fc18dc0bb8feafd4f4af52f54b43f11ba8d77746
Yusin <sunkatabeckama@gmail.com>
2024-10-16 10:59:30 +0200

    View mode enabled

--- client/app/projects/[id]/page.tsx
+++ client/app/projects/[id]/page.tsx
@@ -1,8 +1,12 @@
-"use client"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
 
 import React, { useState } from "react";
 import ProjectHeader from "../ProjectHeader";
-import Board from "../Boardview";
+import Board from "../BoardView";
+import ListView from "../ListView";
+import TimelineView from "../TimelineView";
+import TableView from "../TableView";
 
 type Props = {
   params: { id: string };
@@ -13,14 +17,24 @@ function Project({ params }: Props) {
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
-  return <div>
-    {/* Modal New Tasks */}
-    <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-    {activeTab === "Board" && (
+  return (
+    <div>
+      {/* Modal New Tasks */}
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
-    
-  </div>;
+      {activeTab === "List" && (
+        <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <TimelineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+    </div>
+  );
 }
 
 export default Project;
