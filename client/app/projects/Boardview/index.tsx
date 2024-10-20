d3423c602d6d170c0273847e1c60b8af66ea3dae
Yusin <sunkatabeckama@gmail.com>
2024-10-20 21:08:22 +0200

    Updating missing sign

--- client/app/projects/Boardview/index.tsx
+++ client/app/projects/Boardview/index.tsx
@@ -29,7 +29,7 @@ const BoardView = ({ id, setIsModalNewTaskOpen }: BoardProps) => {
     updateTaskStatus({ taskId, status: toStatus });
   };
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>An error occurred while fetching tasks</div>;
+  if (error) return <div>An error occurred while fetching tasks.</div>;
 
 
   return (
