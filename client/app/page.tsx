35129f77c54854955ab451209801db3879df1f11
Yusin <sunkatabeckama@gmail.com>
2024-10-20 16:19:52 +0200

    Home Page implementation

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,7 @@
+import HomePage from "./home/page"
+
 export default function Home() {
   return (
-      <main className="container h-full w-[100%] bg-gray-100 bg-transparent p-8">
-        HI
-      </main>
+      <HomePage />
   );
 }
